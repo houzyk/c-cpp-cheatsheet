18fb46824a0f6e68518ce50baede587bf231d58f
houzairkoussa <houzairmk@icloud.com>
2022-01-16 11:14:11 +0400

    ADD nested adt

--- adt.c
+++ adt.c
@@ -3,14 +3,23 @@ struct Object {
   char* name;
 };
 
+struct Component
+{
+  struct Object parent;
+  int _number;
+};
+
 // prototyping
 struct Object asn(int, char*);
 
 int main(int argc, char *argv[]) {
-  struct Object unit = asn(1, argv[1]);
+  struct Object unit = asn(4, argv[1]);
   // * ===
   // unit.number = 1;
   // unit.name = argv[1];
+  struct Component unit_component;
+  unit_component.parent = unit;
+  unit_component._number = 2;
   return 0;
 }
 
